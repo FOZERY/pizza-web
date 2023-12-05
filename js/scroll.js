9835f8d1f79ef758f9dc96b1c74ed9a335ebcf6b
fozery <fozery@yandex.ru>
2023-12-05 13:12:33 +0300

    fix: change scroll offset for media <860px

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,16 @@
 function scrollToElement(id) {
     event.preventDefault();
+    const mediaQuery = window.matchMedia('(max-width: 860px)');
 
     let element = document.getElementById(id);
     let navHeight = document.querySelector('.nav').offsetHeight;
-    let offset = element.offsetTop - navHeight;
 
+    let offset;
+    if (mediaQuery.matches) {
+        offset = element.offsetTop;
+    } else {
+        offset = element.offsetTop - navHeight;
+    }
     window.scrollTo({ top: offset, behavior: 'smooth' });
 }
 
